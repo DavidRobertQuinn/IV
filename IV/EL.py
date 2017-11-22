3ea603bc44b7763566dd9532f1da65b8f255155f
DavidRobertQuinn <davidrobertquinn@gmail.com>
2017-11-22 10:13:28 +0000

    Finished EL

--- IV/EL.py
+++ IV/EL.py
@@ -5,6 +5,29 @@ import scipy.misc
 import scipy.ndimage
 from mpl_toolkits.mplot3d import Axes3D
 from skimage.color import rgb2gray
+import os
+
+
+def twoD_coloured_visualisation(image_file):
+    parent_folder = os.path.dirname(image_file)
+    image = scipy.misc.imread(image_file)
+    gray_img = rgb2gray(image)
+    fig, ax = plt.subplots(figsize=(6.4, 4.8))
+    # Set whitespace to 0
+    fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
+    # Display the image
+    ax.imshow(gray_img, extent=(0, 1, 1, 0))
+    # Turn off axes and set axes limits
+    ax.axis('tight')
+    ax.axis('off')
+    figure_folder = os.path.join(parent_folder, 'figures')
+    if not os.path.exists(figure_folder):
+        os.makedirs(figure_folder)
+    file_name = os.path.basename(
+        image_file)[:-4] + '_2D_visualisation' + '.png'
+    save_name = os.path.join(figure_folder, file_name)
+    plt.savefig(save_name, format='png', dpi=600,
+                bbox_inches='tight', pad_inches=0)
 
 
 def oneD_intensity_profile(image_file, x0, y0, x1, y1, num_of_coords_on_line=500, image_ticks=False,
@@ -16,10 +39,12 @@ def oneD_intensity_profile(image_file, x0, y0, x1, y1, num_of_coords_on_line=500
     # Extract the values along the line, using cubic interpolation
     zi = scipy.ndimage.map_coordinates(gray_img, np.vstack((y, x)))
     fig, axes = plt.subplots(nrows=2, figsize=figsize)
-    axes[0].imshow(gray_img)
-    axes[0].plot([x0, x1], [y0, y1], 'ro-')
+    axes[0].imshow(gray_img, )
+    axes[0].plot([x0, x1], [y0, y1], 'ro-', linewidth=2, markersize=5)
     axes[0].axis('image')
-    axes[1].plot(zi)
+    axes[1].plot(zi, linewidth=2)
+    axes[1].set_xticks([])
+    axes[1].set_ylabel("Intensity (a.u) \n along red line")
     if not image_ticks:
         axes[0].set_xticks([])
         axes[0].set_yticks([])
@@ -35,3 +60,24 @@ def oneD_intensity_profile(image_file, x0, y0, x1, y1, num_of_coords_on_line=500
                         '.pdf'), format='pdf', dpi=600, bbox_inches='tight')
     else:
         return fig
+
+
+def el_visualisation(image_file):
+    image = scipy.misc.imread(image_file)
+    gray_img = rgb2gray(image)
+    gray_img = scipy.misc.imresize(gray_img, 0.3, interp='cubic')
+    xx, yy = np.mgrid[0:gray_img.shape[0], 0:gray_img.shape[1]]
+    fig = plt.figure(figsize=(12, 9))
+    ax = fig.gca(projection='3d')
+    label = image_file[:-4]
+    ax.plot_surface(xx, yy, gray_img / np.max(gray_img),
+                    rstride=1, cstride=1, cmap='viridis', linewidth=0)
+    fake2Dline = mpl.lines.Line2D(
+        [0], [0], linestyle="none", c='b', marker='o')
+    plt.axis('off')
+    # ax.legend([fake2Dline], [label], numpoints = 1)
+    ax.view_init(elev=90, azim=-90)
+    plt.savefig(label + '.png', format='png', dpi=600, bbox_inches='tight')
+    ax.view_init(elev=75, azim=-45)
+    plt.savefig(str(label + 'angle' + '.png'),
+                format='png', dpi=600, bbox_inches='tight')
